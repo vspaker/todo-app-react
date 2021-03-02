1e07cb983afb9e9beba5a419d4f8aceadef9b5ba
Семён <hereff@ya.ru>
2021-03-02 21:56:16 +0300

    Реализовал возможность добавления нового задания

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,19 +3,37 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+	state = {
+		label: '',
+	};
+	onLabelChange = (evt) => {
+		this.setState({
+			label: evt.target.value,
+		});
+	};
+	onSubmit = (evt) => {
+		evt.preventDefault();
+		this.props.onItemAdded(this.state.label);
+		this.setState({
+			label: '',
+		});
+	};
 	render() {
 		return (
-			<div className="item-add-form">
-				<button
-					className="btn btn-outline-secondary"
-					onClick={() =>
-						this.props.onItemAdded(
-							`Hello Word!`
-						)
-					}>
-					Add task
+			<form
+				className="item-add-form d-flex"
+				onSubmit={this.onSubmit}>
+				<input
+					type="text"
+					className="form-control"
+					onChange={this.onLabelChange}
+					placeholder="What needs to be done..."
+					value={this.state.label}
+				/>
+				<button className="btn btn-outline-secondary">
+					Add&nbsp;task
 				</button>
-			</div>
+			</form>
 		);
 	}
 }
