0f9ad4f3fde5fe1d55e9b6362d109a18a9fe75ee
Семён <hereff@ya.ru>
2021-03-02 19:46:18 +0300

    Реализовал удаление элементов

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,36 +7,56 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
-	const todoData = [
-		{
-			label: 'Drink Coffee',
-			important: false,
-			id: 1,
-		},
-		{
-			label: 'Make Awesome App',
-			important: true,
-			id: 2,
-		},
-		{
-			label: 'Have a lunch',
-			important: false,
-			id: 3,
-		},
-	];
+export default class App extends Component {
+	state = {
+		todoData: [
+			{
+				label: 'Drink Coffee',
+				important: false,
+				id: 1,
+			},
+			{
+				label: 'Make Awesome App',
+				important: true,
+				id: 2,
+			},
+			{
+				label: 'Have a lunch',
+				important: false,
+				id: 3,
+			},
+		],
+	};
+	deleteItem = (id) => {
+		this.setState(({ todoData }) => {
+			const idx = todoData.findIndex(
+				(el) => el.id === id
+			);
 
-	return (
-		<div className="todo-app">
-			<AppHeader toDo={1} done={3} />
-			<div className="top-panel d-flex">
-				<SearchPanel />
-				<ItemStatusFilter />
+			const updatedData = [
+				...todoData.slice(0, idx),
+				...todoData.slice(idx + 1),
+			];
+			return {
+				todoData: updatedData,
+			};
+		});
+	};
+	render() {
+		const { todoData } = this.state;
+		return (
+			<div className="todo-app">
+				<AppHeader toDo={1} done={3} />
+				<div className="top-panel d-flex">
+					<SearchPanel />
+					<ItemStatusFilter />
+				</div>
+
+				<TodoList
+					todos={todoData}
+					onDeleted={this.deleteItem}
+				/>
 			</div>
-
-			<TodoList todos={todoData} />
-		</div>
-	);
-};
-
-export default App;
+		);
+	}
+}
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,7 +3,7 @@ import React from 'react';
 import TodoListItem from '../todo-list-item';
 import './todo-list.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
 	const elements = todos.map((item) => {
 		const { id, ...itemProps } = item;
 
@@ -11,7 +11,12 @@ const TodoList = ({ todos }) => {
 			<li
 				key={id}
 				className="list-group-item">
-				<TodoListItem {...itemProps} />
+				<TodoListItem
+					{...itemProps}
+					onDeleted={() =>
+						onDeleted(id)
+					}
+				/>
 			</li>
 		);
 	});
